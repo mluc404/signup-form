210bef10fb0c8c12a1d130d421798cd5a52bb066
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-03-07 19:55:59 -0800

    feat: validate form inputs
    - Create Acc button will check for input validation
    - Prevent creating acc if inputs are invalid

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
+// Auto format phone number to (xxx) xxx-xxxx
 let phoneInput = document.querySelector("#phoneNumber");
-
 let arr = [];
-
 phoneInput.addEventListener("input", (e) => {
-  // e.target.value = "(949) 24";
   let digit = e.data;
   if (digit !== null) arr.push(digit);
   e.target.value = "(";
@@ -26,5 +24,49 @@ phoneInput.addEventListener("keydown", (e) => {
   if (key === "Backspace") {
     arr.pop();
     console.log("after backspace", arr);
+  }
+});
+
+// Show red border on invalid inputs
+
+let inputs = document.querySelectorAll("input[required]");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let passwordConfirm = document.querySelector("#passwordConfirm");
+let accButton = document.querySelector("#accButton");
+
+accButton.addEventListener("click", (e) => {
+  let isValid = true;
+  inputs.forEach((input) => {
+    if (!input.value.trim()) {
+      input.style.border = "red solid 2px";
+      isValid = false;
+    } else {
+      input.style.border = "rgba(115, 156, 142) solid 2px";
+    }
+  });
+
+  // check email format
+  if (!email.value.includes("@")) {
+    email.style.border = "red solid 2px";
+    isValid = false;
+  } else {
+    email.style.border = "rgba(115, 156, 142) solid 2px";
+  }
+
+  // check password match
+  if (password.value !== passwordConfirm.value) {
+    password.style.border = "red solid 2px";
+    passwordConfirm.style.border = "red solid 2px";
+    isValid = false;
+  } else {
+    password.style.border = "rgba(115, 156, 142) solid 2px";
+    passwordConfirm.style.border = "rgba(115, 156, 142) solid 2px";
+  }
+  console.log(password.value);
+  console.log(passwordConfirm.value);
+
+  if (!isValid) {
+    e.preventDefault();
   }
 });
